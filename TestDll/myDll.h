3b9af8d209ceb3e4e1ad92bfaee23c1e1d40d553
haoxiangyang <haoxiangyang888@hotmail.com>
2018-04-20 23:13:43 +0800

    add extern "C"

--- TestDll/myDll.h
+++ TestDll/myDll.h
@@ -8,10 +8,19 @@
 #	define DLL_INSTANCE __declspec(dllimport)
 #endif // DLL_INSTANCE
 
+#ifdef __cplusplus
+
+extern "C"
+{
+
+#endif // __cplusplus
 
 
-DLL_INSTANCE int mySum(int a, int b);
+	DLL_INSTANCE int mySum(int a, int b);
 
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 
 #endif // !TEST_DLL
